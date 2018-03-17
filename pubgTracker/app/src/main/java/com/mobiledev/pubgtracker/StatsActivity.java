27c17d147e3fc1ba080180a5f88c66405ea7a272
Curtis Warrick <warrickc@oregonstate.edu>
2018-03-16 17:58:21 -0700

    UI updated. Activities currently hooked to UI, just have to pass JSON between to change.

---
+++ pubgTracker/app/src/main/java/com/mobiledev/pubgtracker/StatsActivity.java
@@ -0,0 +1,18 @@
+package com.mobiledev.pubgtracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by warrickc on 3/16/2018.
+ */
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.stats_activity);
+    }
+}
