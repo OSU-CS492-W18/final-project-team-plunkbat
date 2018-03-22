4314e37a8aabac6abab8389014c2f80372bdc6d9
Curtis Warrick <warrickc@oregonstate.edu>
2018-03-22 13:42:40 -0700

    Search button now opens new intent. Need to still change search function & pass parsed obj to intent as message. Also added TextView stuff to Stats Activity.

--- pubgTracker/app/src/main/java/com/mobiledev/pubgtracker/MainActivity.java
+++ pubgTracker/app/src/main/java/com/mobiledev/pubgtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobiledev.pubgtracker;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.nfc.Tag;
@@ -8,9 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,13 +25,15 @@ import com.mobiledev.pubgtracker.TrackAdapter;
 
 //Main page,search bar allows to search for user and click by name.
 public class MainActivity extends AppCompatActivity implements TrackAdapter.ItemClickListener{
+
     private static final String TAG = MainActivity.class.getSimpleName();
+
     String apiKey = "5cacd83e-3861-42ff-a981-c0c1a81ff6c4";
     String URLTest = "https://api.fortnitetracker.com/v1/profile/pc/AlexRamiGaming";
     String jsonTest;
     TrackAdapter mAdapter;
     SQLiteDatabase mDB;
-    Button searchButton;
+
     SearchContract mSearchContract;
 
     @Override
@@ -39,16 +44,17 @@ public class MainActivity extends AppCompatActivity implements TrackAdapter.Item
         DBHelper dbHelper = new DBHelper(this);
         mDB = dbHelper.getWritableDatabase();
         loadData();
-       // searchButton = (Button)findViewById(R.id.search_button);
-       // searchButton.setOnClickListener(new View.OnClickListener() {
-       //     @Override
-       //     public void onClick(View v) {
-       //         //Still needs a logic for null
-       //         mDB.insert("savedPlayers", "THIS NEEDS TO BE text entered by player", null);
-       //     }
-       // });
 
-        new trackerSearch().execute();
+        ImageButton searchButton = (ImageButton)findViewById(R.id.button_search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent statsIntent = new Intent(MainActivity.this, StatsActivity.class);
+                startActivity(statsIntent);
+                    //mDB.insert("savedPlayers", "THIS NEEDS TO BE text entered by player", null);
+                    //new trackerSearch().execute();
+            }
+        });
     }
 
     @Override
--- pubgTracker/app/src/main/java/com/mobiledev/pubgtracker/StatsActivity.java
+++ pubgTracker/app/src/main/java/com/mobiledev/pubgtracker/StatsActivity.java
@@ -2,6 +2,7 @@ package com.mobiledev.pubgtracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by warrickc on 3/16/2018.
@@ -9,10 +10,36 @@ import android.support.v7.app.AppCompatActivity;
 
 public class StatsActivity extends AppCompatActivity {
 
+
+    private TextView tvPlayerName;
+    private TextView tvTimePlayed;
+    private TextView tvAvgMatchTime;
+    private TextView tvScore;
+    private TextView tvWins;
+    private TextView tvWinPercent;
+    private TextView tvKills;
+    private TextView tvKDRatio;
+    private TextView tvKPMin;
+    private TextView tvKPMatch;
+    private TextView tvScorePerMatch;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.stats_activity);
+
+        tvPlayerName = (TextView)findViewById(R.id.tv_player_name);
+        tvTimePlayed = (TextView)findViewById(R.id.tv_time_played);
+        tvAvgMatchTime = (TextView)findViewById(R.id.tv_avg_match_time);
+        tvScore = (TextView)findViewById(R.id.tv_score);
+        tvWins = (TextView)findViewById(R.id.tv_wins);
+        tvWinPercent = (TextView)findViewById(R.id.tv_win_percent);
+        tvKills = (TextView)findViewById(R.id.tv_kills);
+        tvKDRatio = (TextView)findViewById(R.id.tv_kill_death);
+        tvKPMin = (TextView)findViewById(R.id.tv_kpmin);
+        tvKPMatch = (TextView)findViewById(R.id.tv_kpmatch);
+        tvScorePerMatch = (TextView)findViewById(R.id.tv_score_per_match);
     }
 }
